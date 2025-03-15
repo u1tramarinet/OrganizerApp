16d41fd38acb2a85356e432c87e726925e41fb34
Suzuki Takumi <u1tramarine.takumi@gmail.com>
2025-03-15 19:02:19 +0900

    Enable edge-to-edge display
    The app now utilizes the `enableEdgeToEdge()` function to draw behind system bars, extending the app content to the edges of the screen.

--- app/src/main/java/io/github/u1tramarinet/organizerapp/ui/MainActivity.kt
+++ app/src/main/java/io/github/u1tramarinet/organizerapp/ui/MainActivity.kt
@@ -3,12 +3,14 @@ package io.github.u1tramarinet.organizerapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             OrganizerApp()
         }
