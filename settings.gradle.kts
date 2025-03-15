f832831d542b55e594551041cd2dc8a24d83ee2a
Suzuki Takumi <u1tramarine.takumi@gmail.com>
2025-03-15 19:02:02 +0900

    Enable stable configuration cache and type-safe project accessors
    Update gradle properties to enable configuration cache and parallel execution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,9 @@ dependencyResolutionManagement {
     }
 }
 
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+
 rootProject.name = "OrganizerApp"
 include(":app")
  
