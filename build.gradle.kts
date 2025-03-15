4ba09e42ee36ac3b96dfbe8c74a4d5d689191294
Suzuki Takumi <u1tramarine.takumi@gmail.com>
2025-03-15 17:42:30 +0900

    Update gradle versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.8.2" apply false
+    id("com.android.application") version "8.9.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.21" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.21" apply false
     alias(libs.plugins.hilt) apply false
